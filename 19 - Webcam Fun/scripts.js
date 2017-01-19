974c58ffc985454d2f88b8e6802c4675553f0b5d
Sam O'Connell <soconnell@Sams-MacBook-Pro.local>
2017-01-18 19:43:40 -0600

    mostly finished, need to do the effects

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,50 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream => {
+      // console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(error => {
+      console.error(error);
+    })
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
+  }, 16)
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man"/>`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.length; i+=4) {
+    
+  }
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
